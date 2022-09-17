1301b1f77bef6fa183e0153b2d2eec67c1b9d09a
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-09-17 00:33:04 -0300

    feat: app fully rendering with react

--- src/Posts.js
+++ src/Posts.js
@@ -1,136 +1,78 @@
-function Post(){
+function Post(props){
+    return(
+        <div class="post">
+                    <div class="post-top">
+                        <div class="profile-pic"><img src={props.profilePic} alt="profile picture"/></div>
+                        <div class="username">
+                            <p class="p-user">{props.username}</p>
+                        </div>
+                        <div class="post-options">
+                            <ion-icon class="bttn" name="ellipsis-horizontal"></ion-icon>
+                        </div>
+                    </div>
+                    <div class="content"><img src={props.content} alt="media"/></div>
+                    <div class="post-bottom">
+                        <div class="controls">
+                            <div class="icons-left">
+                                <ion-icon class="bttn icon-heart" name="heart-outline"></ion-icon>
+                                <ion-icon class="bttn icon" name="chatbubble-outline"></ion-icon>
+                                <ion-icon class="bttn icon" name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div class="icons-right">
+                                <ion-icon class="bttn icon-save" name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div class="likes">
+                            <img src={props.likedByPic} alt="user that liked your post"/>
+                            <p>Curtido por <strong>{props.likedByName}</strong> e <strong>outras {props.likes.toLocaleString('pt-BR')} pessoas</strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+    )
 
 }
 
 export default function Posts(){
+    const postsData =[
+        {
+            username: "meowed",
+            profilePic: "imgs/profiles/meowed 4.png",
+            content: "imgs/feed/gato-telefone 1.png",
+            likedByName: "adorable_animals",
+            likedByPic: "imgs/profiles/adorableanimals 2.png",
+            likes: 99159
+        },
+        {
+            username: "barked",
+            profilePic: "imgs/profiles/barked 2.png",
+            content: "imgs/feed/dog 1.png",
+            likedByName: "meltedvideos",
+            likedByPic: "imgs/profiles/Melted.jpg",
+            likes: 69329
+        },
+        {
+            username: "meltedvideos",
+            profilePic: "imgs/profiles/Melted.jpg",
+            content: "imgs/feed/meltedProblem.jpg",
+            likedByName: "respondeai",
+            likedByPic: "imgs/profiles/respondeai 2.png",
+            likes: 103420
+        },
+        {
+            username: "meltedvideos",
+            profilePic: "imgs/profiles/Melted.jpg",
+            content: "imgs/feed/meltedSims.jpg",
+            likedByName: "bad.vibes.memes",
+            likedByPic: "imgs/profiles/badvibesmemes 1.png",
+            likes: 69321
+        },
+        
+    ]
+
     return(
         <div class="feed">
-                <div class="post">
-                    <div class="post-top">
-                        <div class="profile-pic"><img src="imgs\profiles\meowed 4.png"/></div>
-                        <div class="username">
-                            <p class="p-user">meowed</p>
-                        </div>
-                        <div class="post-options">
-                            <ion-icon class="bttn" name="ellipsis-horizontal"></ion-icon>
-                        </div>
-                    </div>
-                    <div class="content"><img src="imgs\feed\gato-telefone 1.png" /></div>
-                    <div class="post-bottom">
-                        <div class="controls">
-                            <div class="icons-left">
-                                <ion-icon class="bttn icon-heart" name="heart-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="chatbubble-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div class="icons-right">
-                                <ion-icon class="bttn icon-save" name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-                        <div class="likes">
-                            <img src="imgs\profiles\adorableanimals 2.png"/>
-                            <p>Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="post">
-                    <div class="post-top">
-                        <div class="profile-pic"><img src="imgs\profiles\barked 2.png"/></div>
-                        <div class="username">
-                            <p class="p-user">barked</p>
-                        </div>
-                        <div class="post-options">
-                            <ion-icon class="bttn" name="ellipsis-horizontal"></ion-icon>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <img src="imgs\feed\dog 1.png" />
-                    </div>
-                    <div class="post-bottom">
-                        <div class="controls">
-                            <div class="icons-left">
-                                <ion-icon class="bttn icon-heart" name="heart-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="chatbubble-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div class="icons-right">
-                                <ion-icon class="bttn icon-save" name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-                        <div class="likes">
-                            <img src="imgs\profiles\Melted.jpg"/>
-                            <p>Curtido por <strong>meltedvideos</strong> e <strong>outras 65.329 pessoas</strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="post">
-                    <div class="post-top">
-                        <div class="profile-pic"><img src="imgs\profiles\Melted.jpg"/></div>
-                        <div class="username">
-                            <p class="p-user">meltedvideos</p>
-                        </div>
-                        <div class="post-options">
-                            <ion-icon class="bttn" name="ellipsis-horizontal"></ion-icon>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <img src="imgs\feed\meltedProblem.jpg" />
-                    </div>
-                    <div class="post-bottom">
-                        <div class="controls">
-                            <div class="icons-left">
-                                <ion-icon class="bttn icon-heart" name="heart-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="chatbubble-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div class="icons-right">
-                                <ion-icon class="bttn icon-save" name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-                        <div class="likes">
-                            <img src="imgs\profiles\respondeai 2.png"/>
-                            <p>Curtido por <strong>respondeai</strong> e <strong>outras 103.420 pessoas</strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="post">
-                    <div class="post-top">
-                        <div class="profile-pic"><img src="imgs\profiles\Melted.jpg"/></div>
-                        <div class="username">
-                            <p class="p-user">meltedvideos</p>
-                        </div>
-                        <div class="post-options">
-                            <ion-icon class="bttn" name="ellipsis-horizontal"></ion-icon>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <video width="100%" height="auto" autoplay muted loop>
-                            <source src="imgs\feed\meltedGato.MP4" type="video/mp4"/>
-                            <source src="imgs\feed\meltedGato.ogv" type="video/ogg"/>
-                            Seu browser não suporta a tag video.
-                        </video>
-                    </div>
-                    <div class="post-bottom">
-                        <div class="controls">
-                            <div class="icons-left">
-                                <ion-icon class="bttn icon-heart" name="heart-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="chatbubble-outline"></ion-icon>
-                                <ion-icon class="bttn icon" name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div class="icons-right">
-                                <ion-icon class="bttn icon-save" name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-                        <div class="likes">
-                            <img src="imgs\profiles\badvibesmemes 1.png"/>
-                            <p>Curtido por <strong>bad.vibes.memes</strong> e <strong>outras 69.321 pessoas</strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        {postsData.map(Post)}
+        </div>
     )
 }
